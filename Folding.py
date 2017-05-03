403deb058c98506623e00b381ac418e23ec5b796
Andy Mitchell <me@themitchell.co.uk>
2017-05-03 13:39:35 +0100

    Lets stop using code folding for now

--- Folding.py
+++ Folding.py
@@ -1,5 +1,5 @@
-import sublime, sublime_plugin
+# import sublime, sublime_plugin
 
-class Folding(sublime_plugin.EventListener):
-    def on_load(self, view):
-        view.run_command("fold_by_level", {"level": 2})
+# class Folding(sublime_plugin.EventListener):
+#    def on_load(self, view):
+#        view.run_command("fold_by_level", {"level": 2})
